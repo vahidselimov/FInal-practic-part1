c29b4e6a83366acde8f05768207b73c6745a6d3e
vahidselimov <100331397+vahidselimov@users.noreply.github.com>
2022-05-15 07:38:32 +0400

    Task

---
+++ Military Wor/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Military_Wor
+{
+    internal class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+            int bulletCapacity;
+            int countBullets;
+            bool check = true;
+            do
+            {
+                Console.WriteLine("Please Enter BulletCapacity");
+                bulletCapacity = Convert.ToInt32(Console.ReadLine());
+            } while (bulletCapacity < 0 || bulletCapacity == 0||bulletCapacity>=51);
+            Console.WriteLine("\n ***********\n");
+            do
+            {
+                Console.WriteLine("Please enter CountCapacity...Warning The number of bullets in a weapon must be less than the number of bullets in unit!!");
+                countBullets = Convert.ToInt32(Console.ReadLine());
+                 
+                
+            } while (countBullets < 0 || countBullets > bulletCapacity || countBullets == 0||countBullets>=50);
+            Weapon weapon = new Weapon(bulletCapacity, countBullets, check);
+            weapon.Bulletcapacity = bulletCapacity;
+            weapon.Bulletcapacity = countBullets;
+
+
+            do
+            {
+                Console.WriteLine("0: İnformasiya almaq üçün");
+                Console.WriteLine("1: Shoot metodu üçün");
+                Console.WriteLine("2: Fire metodu üçün");
+                Console.WriteLine("3: GetRemainBulletCount metodu üçün");
+                Console.WriteLine("4: Reload metodu üçün");
+                Console.WriteLine("5: ChangeFireMode metodu üçün");
+                Console.WriteLine("6: Proqramdan dayandırmaq üçün");
+
+                switch (Console.ReadLine())
+                {
+                    case "0":
+                        break;
+                    case "1":
+                        weapon.shoot();
+                        break;
+                    case "2":
+                        weapon.Fire();
+                        break;
+                    case "3":
+                        Console.WriteLine(weapon.GetRemainBulletCount());
+                        break;
+                    case "4":
+                        weapon.Reload();
+                        break;
+                    case "5":
+                        weapon.ChangeFireMode();
+                        break;
+                    case "6":
+                        check = false;
+                        break;
+                    default:
+                        break;
+                }
+            } while (check);
+
+
+        }
+    }
+}
---
+++ Military Wor/Weapon.cs
@@ -0,0 +1,102 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Military_Wor
+{
+    internal class Weapon
+    {
+        private int _bulletcapacity;
+        public  int Bulletcapacity
+        {
+            get
+            {
+                return _bulletcapacity;
+            }
+            set
+            {
+                if (value > 0 && value <= 50)
+                {
+                    _bulletcapacity = value;
+                }
+
+            }
+
+        }
+        
+        public int CountBullets
+        {
+            get
+            {
+                return _countbullets;
+            }
+            set
+            {
+                if (value<0 &&value<=49)
+                {
+                    _countbullets = value;
+                }
+            }
+        }
+
+        private int _countbullets;
+        public bool mood;
+      
+        public Weapon(int bulletcapacity, int countbullets,bool Mood)
+        {
+            Bulletcapacity = bulletcapacity;
+            CountBullets = countbullets;
+            mood = Mood;
+
+        }
+        public void shoot()
+        {
+            if (Bulletcapacity>0)
+            {
+                Console.WriteLine(--Bulletcapacity);
+                
+            }
+            
+            
+
+        }
+       
+        public void Fire()
+        {
+            if (mood==true)
+            {
+                Console.WriteLine(--Bulletcapacity);
+
+               
+            }
+            else
+            {
+                Console.WriteLine(Bulletcapacity = 0);
+            }
+                   
+        }
+        public int GetRemainBulletCount()
+        {
+            return Bulletcapacity - CountBullets;
+        }
+        public int Reload() {
+            return Bulletcapacity += GetRemainBulletCount();
+        }
+        public void ChangeFireMode()
+        {
+            if (mood==true)
+            {
+                Console.WriteLine("Static");
+            }
+            else
+            {
+                Console.WriteLine("Automatic");
+            }
+        }
+    }
+
+    
+      
+  
+}
